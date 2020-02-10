02dfc759cee24400b2a0b16bcee64b0092cfb750
Fernando López <fernando.lparra@alumnos.udg.mx>
2020-02-10 13:54:09 -0600

    Se implementó el módulo productos.

--- AuctionGame_Aministrador/AuctionGame_Aministrador/ChildMainProducts.cs
+++ AuctionGame_Aministrador/AuctionGame_Aministrador/ChildMainProducts.cs
@@ -16,5 +16,125 @@ namespace AuctionGame_Aministrador
         {
             InitializeComponent();
         }
+
+        private void childMenuProductos_Load(object sender, EventArgs e)
+        {
+            UpdateProducts("");
+
+        }
+
+        public void UpdateProducts(string query)
+        {
+            if (string.IsNullOrEmpty(query))
+                query = "SELECT product.idProduct, product.nameProduct, product.startinPrice, product.points, CONVERT(pathImage USING UTF8) FROM product";
+            var products = DB_CONNECTION.consultar_datos(query); //Obtiene una consulta de los productos de la base de datos
+            dgvProducts.Rows.Clear();//Limpia las filas de la tabla producto
+            if (products == null) return;
+            var bProducts = int.TryParse(dgvProducts.Tag.ToString(), out var currentIdProduct);
+
+            foreach (DataRow row in products.Rows)//Agrega todas las filas de la consulta a la tabla
+            {
+
+
+                dgvProducts.Rows.Add((int)row[0], row[1], (decimal)row[2], (int)row[3], $@"{row[4]}");
+                var currentRow = dgvProducts.Rows[dgvProducts.Rows.Count - 1];
+                if (!bProducts || currentIdProduct == -1) continue;
+                if (currentIdProduct == int.Parse(currentRow.Cells[0].Value.ToString()))
+                    currentRow.Selected = true;
+            }
+        }
+        private bool Question(string question, string caption)
+        {
+            return MessageBox.Show(question, caption, MessageBoxButtons.YesNoCancel) == DialogResult.Yes;
+        }
+
+        private void btnDeleteProduct_Click(object sender, EventArgs e)
+        {
+            var row = dgvProducts.CurrentRow; //Obtiene la fila seleccionada
+            if (row == null || row.Index == -1) return;
+            var currentIdproduct = int.Parse(row.Cells[0].Value.ToString());
+            var currentNameProduct = row.Cells[1].Value.ToString();
+
+
+            if (!Question($@"¿Está seguro de eliminar el producto {currentNameProduct} ({currentIdproduct})?",
+                @"Estás a punto de eliminar un producto")) return;
+            var query = $"DELETE FROM product WHERE idProduct = {currentIdproduct}"; //Query  para eliminar producto seleccionado
+            if (DB_CONNECTION.ejecutar(query))//Ejecuta el query en la base de datos
+            {
+                MessageBox.Show(@"Se eliminó correctamente");
+                UpdateProducts("");
+            }
+            else
+            {
+                MessageBox.Show(@"Ocurrió un error al eliminar el producto");
+            }
+        }
+
+        private void dgvProducts_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > -1 && dgvProducts.Rows[e.RowIndex].Selected)
+                dgvProducts.Tag = dgvProducts.Rows[e.RowIndex].Cells[0].Value.ToString();
+        }
+
+        private void dgvProducts_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var row = dgvProducts.CurrentRow; //Obtiene la fila seleccionada
+
+            if (row == null || row.Index == -1 || e.RowIndex == -1) return;
+
+            //Obtiene los datos de la fila seleccioanda y la convierte a objeto de PRODUCTO
+            var idProduct = int.Parse(row.Cells[0].Value.ToString());
+            var nameProduct = row.Cells[1].Value.ToString();
+            var initialPrice = decimal.Parse(row.Cells[2].Value.ToString());
+            var pointsValue = int.Parse(row.Cells[3].Value.ToString());
+            var image = DataControl.Base64StringToImage(row.Cells[4].Value.ToString());
+
+            var product = new Product
+            {
+                IdProduct = idProduct,
+                Name = nameProduct,
+                Price = initialPrice,
+                Points = pointsValue,
+                ImageProduct = image
+            };
+
+            var form = new FrmProduct(product, this);//Instancia un nuevo formulario para agregar productos
+            form.Show();//Abre el formulario
+        }
+
+        private void btnSearchProducts_Click(object sender, EventArgs e)
+        {
+            var option = cboSearchProducts.SelectedIndex;
+            var query = "SELECT product.idProduct, product.nameProduct, product.startinPrice, product.points, CONVERT(pathImage USING UTF8) FROM product";
+            switch (option)
+            {
+                case 1:
+                    query += $" WHERE product.idProduct = {txbSearchProducts.Text}";
+                    break;
+                case 2:
+                    query += $" WHERE product.nameProduct LIKE '{txbSearchProducts.Text}%'";
+                    break;
+                case 3:
+                    query += $" WHERE product.startinPrice = {txbSearchProducts.Text}";
+                    break;
+                case 4:
+                    query += $" WHERE product.points = {txbSearchProducts.Text}";
+                    break;
+            }
+
+            UpdateProducts(query);
+        }
+
+        private void txbSearchProducts_Enter(object sender, EventArgs e)
+        {
+            DataControl.PlaceHolder_Enter((TextBox)sender);
+        }
+
+        private void txbSearchProducts_Leave(object sender, EventArgs e)
+        {
+            DataControl.placeHolder_Leave((TextBox)sender);
+
+        }
+
     }
 }
--- AuctionGame_Aministrador/AuctionGame_Aministrador/FrmProduct.cs
+++ AuctionGame_Aministrador/AuctionGame_Aministrador/FrmProduct.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,188 @@ namespace AuctionGame_Aministrador
 {
     public partial class FrmProduct : Form
     {
+        private readonly Product _product;
+        private readonly bool _edit;
+        private readonly ChildMainProducts _father;
+        public FrmProduct(Form f)
+        {
+            InitializeComponent();
+            _edit = false;
+            _father = (ChildMainProducts)f;
+
+        }
         public FrmProduct()
         {
             InitializeComponent();
+            _edit = false;
+        }
+        public FrmProduct(Product p, Form f)
+        {
+            InitializeComponent();
+            _product = p;
+            _edit = true;
+            _father = (ChildMainProducts)f;
+        }
+
+        private void frmProducto_Load(object sender, EventArgs e)
+        {
+            if (_product != null)
+            {
+                LoadProduct();
+            }
+        }
+
+        private void LoadProduct()
+        {
+            DataControl.Text(txbIdProduct, _product.IdProduct.ToString());
+            DataControl.Text(txbNameProduct, _product.Name);
+            DataControl.Text(txbInitialPrice, _product.Price.ToString(CultureInfo.InvariantCulture));
+            DataControl.Text(txbPointsValue, _product.Points.ToString(CultureInfo.InvariantCulture));
+            pboxProducto.Image = _product.ImageProduct;
+        }
+
+        protected virtual bool Question(string question, string caption)
+        {
+            return MessageBox.Show(question, caption, MessageBoxButtons.YesNoCancel) == DialogResult.Yes;
+        }
+
+        private void Txb_Enter(object sender, EventArgs e)
+        {
+            DataControl.PlaceHolder_Enter((TextBox)sender);
+        }
+
+        private void Txb_Leave(object sender, EventArgs e)
+        {
+            DataControl.placeHolder_Leave((TextBox)sender);
+        }
+
+
+        private void Txb_Validated(object sender, EventArgs e)
+        {
+            try
+            {
+                var txb = (TextBox)sender;
+                var txbTag = (string)txb.Tag;
+                var type = txbTag.Split(',')[1];
+                DataControl.Validar(txb, type);
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+            }
+        }
+
+        private bool ValidData()
+        {
+            var textboxes = new object[] { txbIdProduct, txbInitialPrice, txbNameProduct, txbPointsValue };
+            return DataControl.Validar(textboxes);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (ValidData()) //Valida Datos
+            {
+                var newIdProduct = txbIdProduct.Text;
+                var newNameProduct = txbNameProduct.Text;
+                var newInitialPrice = txbInitialPrice.Text;
+                var newPointsValue = txbPointsValue.Text;
+                var newImageB64 = DataControl.ImageToBase64String(pboxProducto.Image);
+
+                if (!_edit) //Si no va a editar un producto ya existente
+                {
+                    if (DB_CONNECTION.existencia($@"SELECT * FROM product WHERE idProduct = {newIdProduct} LIMIT 1", 1))
+                    {
+                        MessageBox.Show(@"El código introducido ya existe.");
+                        return;
+                    }
+                    var query =
+                        $"INSERT INTO product (idProduct, nameProduct, startinPrice, points, pathImage) " +
+                        $"VALUES (" +
+                        $"{newIdProduct}," +
+                        $"'{newNameProduct}'," +
+                        $"{newInitialPrice}, " +
+                        $"{newPointsValue}, " +
+                        $"'{newImageB64}')";
+                    if (DB_CONNECTION.ejecutar(query)) //Si se ejecuta la consulta en la base de datos correctamente
+                    {
+                        _father?.UpdateProducts("");
+                        if (MessageBox.Show(@"¡Se ha registrado el producto exitosamente! ¿Desea agregar otro?",
+                                @"Producto agregado", MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==
+                            DialogResult.Yes)
+                        {
+
+                            txbIdProduct.Clear();
+                            txbNameProduct.Clear();
+                            txbInitialPrice.Clear();
+                            txbPointsValue.Clear();
+                            pboxProducto.Image = Image.FromFile("../../Resources/no-foto.png");
+                        }
+                        else
+                        {
+
+                            this.Close();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(@"Ocurrió un problema al registrar el producto.");
+                    }
+                }
+                else if (_edit) //Si va a editar un producto ya existente
+                {
+                    if (DB_CONNECTION.existencia($@"SELECT * FROM product WHERE idProduct = {newIdProduct} LIMIT 2", 2))
+                    {
+                        MessageBox.Show(@"El código introducido ya existe.");
+                        return;
+                    }
+                    var query =
+                        $"UPDATE product set " +
+                        $"idProduct = {newIdProduct}, " +
+                        $"nameProduct = '{newNameProduct}', " +
+                        $"startinPrice = {newInitialPrice}, " +
+                        $"points = {newPointsValue}, " +
+                        $"pathImage = '{newImageB64}' " +
+                        $"WHERE idProduct = {_product.IdProduct}";
+                    if (DB_CONNECTION.ejecutar(query)) //Si se ejecuta la consulta en la base de datos correctamente
+                    {
+                        _father?.UpdateProducts("");
+                        if (!Question(@"¡Se ha modificado el producto exitosamente! ¿Desea Salir?",
+                            @"Producto Modificado")) return;
+                        this.Close();
+                    }
+                    else
+                    {
+                        MessageBox.Show(@"Ocurrió un problema modificando el producto.");
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show(@"Existe un error en el formulario.");
+            }
+        }
+
+        private void btnPatchImage_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Multiselect = false;
+                ofd.Filter = @"Archivos de Imagen (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    using (Image img = Image.FromFile(ofd.FileName))
+                    {
+                        pboxProducto.Image = (Image)img.Clone();
+                    }
+                }
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            if (Question("¿Realmente desea salir?", "Está a punto de salir"))
+                this.Close();
         }
     }
 }
