02c7b34e9d8c884e82780ca33d1fa204c9586bfd
Fernando López <fernando.lparra@alumnos.udg.mx>
2020-02-10 15:03:23 -0600

    Cambios en el nombre del proyecto

--- AuctionGame_Admin/AuctionGame_Admin/Properties/Resources.Designer.cs
+++ AuctionGame_Admin/AuctionGame_Admin/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AuctionGame_Aministrador.Properties {
+namespace AuctionGame_Admin.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace AuctionGame_Aministrador.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AuctionGame_Aministrador.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AuctionGame_Admin.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
